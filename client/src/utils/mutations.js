f46c84960af03b5dff94ffeaad0bab4502c0a57d
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-08-06 21:06:14 -0500

    feat: add client side login/signup functionality

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+/* DEPENDENCIES */
+import { gql } from "@apollo/client";
+
+/* MUTATIONS */
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
